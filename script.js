4f2e00a77789ba15cb403a294a1e6cedd11f7f86
digambernegi <digambernegi001@gmail.com>
2022-03-14 18:28:42 +0530

    added try catch block to fetch and alt attribute to img

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const advicepara = document.querySelector(".advice");
 const spindice = document.querySelector(".dice__container");
 
 async function getdata() {
+  try{
   let response = await fetch(api);
   let data = await response.json();
   return data;
+  }
+  catch(err){
+    console.log(err)
+  }
 }
 
-function randomno() {
+//In case you need random no.
+/* function randomno() {
   return Math.floor(Math.random() * 1000 + 1);
-}
+} */
 
 spindice.addEventListener("click", () => {
-  getdata().then((data) => (advicepara.textContent = data.slip.advice));
-  adviceno.textContent = randomno();
+  getdata().then(
+    (data) => (
+      (advicepara.textContent = data.slip.advice),
+      (adviceno.textContent = data.slip.id)
+    )
+  );
+
+  /* adviceno.textContent = randomno(); */
 });
